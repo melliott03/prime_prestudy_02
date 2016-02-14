7bd388b7254b302677e90b5aad339ddda4f57fa4
melliott03 <melliott03@gmail.com>
2016-02-14 15:39:48 -0600

    initial commit

---
+++ ObjectAssessment.js
@@ -0,0 +1,35 @@
+avengersMovie = {
+    name: "Avengers",
+    runtime: "143 mins",
+    releaseYear: 2012,
+    director: "Joss Whedon"
+};
+console.log(avengersMovie);
+
+
+
+
+
+/*
+
+For this assessment, download the file located here: http://goo.gl/EhXfQq
+
+The file is a Javascript file (You can use SublimeText2 to open it!). 
+In it, there is an object that is being declared using the Object Literal method. 
+However, err.... there is a problem. Actually, lots of them. 
+Your task this week is to clean up this object, making sure all the proper 
+syntax and other needed components are included.
+
+Once you have worked through the Object and corrected it, 
+create an html file that pulls in the Javascript file and console logs out the object. 
+Once you have completed that, check the project into GitHub under a repo called 'prime_prestudy_02'.
+
+
+avengersMovie = {
+    name : Avengers;
+    runtime : 143 mins;
+    releaseYear = 2012;
+    "Director" : "Joss Whedon".
+}
+
+*/
